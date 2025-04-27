f7e2164d85a52ac294ca0a2dd9abb73d35e16005
Yap Xin Yu <xinyuxu1204@gmail.com>
2025-04-27 18:43:45 +0800

    Confession

---
+++ Confession/StrangersGossip/fullpost_system/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('post/<int:post_id>/', views.full_post, name='full_post'),
+    path('post/<int:post_id>/like/', views.like_post, name='like_post'),
+    path('bookmark/', views.bookmark_view, name='bookmark_page'),
+    path('post/<int:post_id>/save/', views.toggle_save_post, name='toggle_save_post'),
+    path('post/<int:post_id>/report/', views.report_post, name='report_post'),
+    path('post/<int:post_id>/comment/', views.comment_post, name='comment_post'),
+    path('comment/<int:comment_id>/like/', views.like_comment, name='like_comment'),
+    path('reply/comment/<int:comment_id>/', views.reply_comment, name='reply_comment'),
+    path('reply/<int:reply_id>/like/', views.like_reply, name='like_reply'),
+    path('comment/<int:comment_id>/report/', views.report_comment, name='report_comment'),
+    path('comment/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
+    path('report/done/', views.done_report, name='done_report'),
+]
