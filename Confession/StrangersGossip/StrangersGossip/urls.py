73fca119f701efaa9475799c1cbdc97968201f76
Yap Xin Yu <xinyuxu1204@gmail.com>
2025-04-28 02:40:19 +0800

    delete Confession

--- Confession/StrangersGossip/StrangersGossip/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from django.contrib import admin
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('fullpost_system.urls')),
-    path('', include('fullpost_system.urls')),
-]
