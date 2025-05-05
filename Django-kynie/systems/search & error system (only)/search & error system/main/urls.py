cb81ab85d3ce1154366c6fd88f23712db6f81359
ANG <kynieang2006@gmail.com>
2025-05-05 22:09:07 +0800

    add error designs

---
+++ Django-kynie/systems/search & error system (only)/search & error system/main/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index_view, name='home'),
+    path('search/', views.search_view, name='search'),
+    path('trending/', views.trending_view, name='trending'),
+    path('post/', views.post_view, name='post'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('notification/', views.notification_view, name='notification'),
+    path('bookmark/', views.bookmark_view, name='bookmark'),
+    path('post/<int:post_id>/', views.full_post_view, name='full_post'),
+    path('broken/', views.broken_view, name='broken'),
+    
+]
