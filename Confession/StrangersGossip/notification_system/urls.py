5b46c795db98bb3281ea2dc31e6557e1c1a0909e
Yap Xin Yu <xinyuxu1204@gmail.com>
2025-05-07 18:51:40 +0800

    delete notification_system

--- Confession/StrangersGossip/notification_system/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from notification_system import views
-
-urlpatterns = [
-    path('api/notifications/', views.notification_list, name='notification-list'),
-    path('api/notifications/<int:notification_id>/read/', 
-         views.mark_as_read, name='mark-as-read'),
-]
