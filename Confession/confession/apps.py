929ff568362b018102df2903b24b10b716c05bde
Yap Xin Yu <xinyuxu1204@gmail.com>
2025-06-12 17:21:55 +0800

    remove files

--- Confession/confession/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class ConfessionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'confession'
-
-    def ready(self):
-        import confession.signals  # 👈 确保加载 signal
